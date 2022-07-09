8aefff3ec42e1602ca8c09419cb7cac215bd9294
JaedynB <jaedyn.brown@mavs.uta.edu>
2022-07-09 13:50:08 -0500

    header changes

--- frontent_react/src/container/Header/Header.jsx
+++ frontent_react/src/container/Header/Header.jsx
@@ -4,6 +4,19 @@ import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Header.scss';
 
+const scaleVariants = {
+  whileInView: 
+  {
+    scale: [0, 1],
+    opacity: [0,1],
+    transition: 
+    {
+      duration: 1,
+      ease:'easeInOut'
+    }
+  }
+}
+
 const Header = () => {
   return (
     <div className = "app__header app__flex">
@@ -31,12 +44,23 @@ const Header = () => {
       <motion.div
         whileInView= {{ opacity: [0, 1] }}
         transition= {{ duration: 0.5, delayChildren: 0.5 }}
-        className= "app__header-info"
+        className= "app__header-img"
       >
-
+        <img src={images.giraffe} alt="profile_bg" />
+        <motion.img
+          whileInView= {{ scale: [0, 1] }}
+          transition= {{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle"
+        />
       </motion.div>
 
-      <motion.div>
+      <motion.div
+        variant= {scaleVariants}
+        whileInView= {scaleVariants.whileInView}
+        className= "app__header-circles"
+      >
 
       </motion.div>
     </div>
