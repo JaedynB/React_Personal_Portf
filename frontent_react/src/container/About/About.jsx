911d38254dce84507683408d8756e39d37393c3f
JaedynB <jaedyn.brown@mavs.uta.edu>
2022-07-11 17:12:32 -0500

    connect sanity to react

--- frontent_react/src/container/About/About.jsx
+++ frontent_react/src/container/About/About.jsx
@@ -3,15 +3,25 @@ import { motion } from 'framer-motion';
 
 import { images } from '../../constants'
 import './About.scss';
+import { urlFor, client } from '../../client';
 
 const abouts = [
   { title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01},
   { title: 'Web Design', description: 'I am a good web developer.', imgUrl: images.about02},
   { title: 'UI/UX', description: 'I am a good web developer.', imgUrl: images.about03},
   { title: 'Web Animations', description: 'I am a good web developer.', imgUrl: images.about04}
-]
+];
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+  }, []);
+  
   return (
     <>
       <h2 className= "head-text">I Know That <span>Good Development</span><br />means<span> Good Business</span></h2>
